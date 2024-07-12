d8d47c870cb3ae4d924c320dc21699a08ae38130
Hampus Avekvist <hampus.avekvist@hey.com>
2024-07-12 05:38:04 +0200

    feat: let examples fail if no cocktail is found

--- thecocktaildb-rs/examples/by_name.rs
+++ thecocktaildb-rs/examples/by_name.rs
@@ -3,7 +3,7 @@ use thecocktaildb_rs::{Client, Cocktail};
 #[tokio::main]
 async fn main() -> Result<(), thecocktaildb_rs::Error> {
     let client = Client::new("1")?;
-    let result = Cocktail::by_name(&client, "margarita").await;
+    let result = Cocktail::by_name(&client, "margarita").await?;
     println!("{:?}", result);
     Ok(())
 }
