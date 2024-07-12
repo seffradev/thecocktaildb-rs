cf1337e14a6d393145412162fbaa7173d7faf43e
Hampus Avekvist <hampus.avekvist@hey.com>
2024-07-12 06:32:27 +0200

    feat: implement all Glasses endpoints

---
+++ thecocktaildb-rs/src/glasses.rs
@@ -0,0 +1,46 @@
+use derive_more::Deref;
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+use crate::{Client, Error};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct GlassDto {
+    #[serde(rename = "strGlass")]
+    alcoholic: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct GlassesDto {
+    drinks: Vec<GlassDto>,
+}
+
+#[derive(Debug, Deref)]
+pub struct Glass(String);
+
+impl Glass {
+    /// List the glasses
+    #[instrument]
+    pub async fn list(client: &Client) -> Result<Glasses, Error> {
+        Ok(reqwest::get(client.base_url.join("list.php?g=list")?.to_string())
+            .await?
+            .json::<GlassesDto>()
+            .await?
+            .into())
+    }
+}
+
+impl From<GlassDto> for Glass {
+    fn from(value: GlassDto) -> Self {
+        Self(value.alcoholic)
+    }
+}
+
+#[derive(Debug, Deref)]
+pub struct Glasses(Vec<Glass>);
+
+impl From<GlassesDto> for Glasses {
+    fn from(value: GlassesDto) -> Self {
+        Self(value.drinks.into_iter().map(Into::into).collect())
+    }
+}
--- thecocktaildb-rs/src/lib.rs
+++ thecocktaildb-rs/src/lib.rs
@@ -4,11 +4,13 @@ use thiserror::Error;
 mod alcoholics;
 mod client;
 mod cocktails;
+mod glasses;
 mod ingredients;
 
 pub use alcoholics::Alcoholic;
 pub use client::Client;
 pub use cocktails::Cocktail;
+pub use glasses::Glass;
 pub use ingredients::Ingredient;
 
 #[derive(Debug, Display, Error)]
