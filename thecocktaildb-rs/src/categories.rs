f7a3f67b2e3dea4460530f2eb0e8dcbd40942606
Hampus Avekvist <hampus.avekvist@hey.com>
2024-07-12 06:34:57 +0200

    feat: implement all Categories endpoints

---
+++ thecocktaildb-rs/src/categories.rs
@@ -0,0 +1,46 @@
+use derive_more::Deref;
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+use crate::{Client, Error};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct CategoryDto {
+    #[serde(rename = "strCategory")]
+    alcoholic: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct CategoriesDto {
+    drinks: Vec<CategoryDto>,
+}
+
+#[derive(Debug, Deref)]
+pub struct Category(String);
+
+impl Category {
+    /// List the categories
+    #[instrument]
+    pub async fn list(client: &Client) -> Result<Categories, Error> {
+        Ok(reqwest::get(client.base_url.join("list.php?c=list")?.to_string())
+            .await?
+            .json::<CategoriesDto>()
+            .await?
+            .into())
+    }
+}
+
+impl From<CategoryDto> for Category {
+    fn from(value: CategoryDto) -> Self {
+        Self(value.alcoholic)
+    }
+}
+
+#[derive(Debug, Deref)]
+pub struct Categories(Vec<Category>);
+
+impl From<CategoriesDto> for Categories {
+    fn from(value: CategoriesDto) -> Self {
+        Self(value.drinks.into_iter().map(Into::into).collect())
+    }
+}
--- thecocktaildb-rs/src/lib.rs
+++ thecocktaildb-rs/src/lib.rs
@@ -2,12 +2,14 @@ use derive_more::Display;
 use thiserror::Error;
 
 mod alcoholics;
+mod categories;
 mod client;
 mod cocktails;
 mod glasses;
 mod ingredients;
 
 pub use alcoholics::Alcoholic;
+pub use categories::Category;
 pub use client::Client;
 pub use cocktails::Cocktail;
 pub use glasses::Glass;
